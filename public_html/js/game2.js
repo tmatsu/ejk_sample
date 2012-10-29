06d8c156b2f3bca937c5c6afa9d04318e706c106
kakusan <kakukaku60@gmail.com>
2012-10-30 02:22:37 +0900

    added code

--- public_html/js/game2.js
+++ public_html/js/game2.js
@@ -5,24 +5,22 @@ enchant();
 window.onload = function() {
 
   // ゲームクラスのインスタンス
-  var game = new Game(300, 300);
+  game = new Game(300, 300);
 
   game.preload('img/space3.gif');
 
   game.onload = function() {
-    var scene = new Scene();
 
     // キャラクターのスプライトを生成
-//    var character = new Sprite(32, 32);
-    // キャラクターに画像をセットする
-    character.image = game.assets['img/space3.gif'];
-    // キャラクターをSceneオブジェクトに追加する
-    scene.addChild(character);
+    x = Math.random() * 300;
+    y = 0;
+    var Kuma = new KumaSprite(x, y);
 
-    game.pushScene(scene);
+    // キャラクターをSceneオブジェクトに追加する
+    game.rootScene.addChild(Kuma);
   }
 
-  game.rootScene.backgroundColor = 'rgb(0, 0, 0)';
+  game.rootScene.backgroundColor = 'rgb(240, 250, 250)';
 
   // ゲームを開始
   game.start();
@@ -31,23 +29,26 @@ window.onload = function() {
 /**
  * Kumaスプライトクラス
  */
-var KumaSprite = enchant.Class.create(Sprite, {
+var KumaSprite = Class.create(Sprite, {
   initialize : function(x, y) {
 
     // 親クラスの呼び出し
     Sprite.call(this, 32, 32);
+    // キャラクターに画像をセットする
     this.image = game.assets['img/space3.gif'];
 
     this.x = x;
     this.y = y;
     this.frame = 1;
-
-    // Kumaを画面下まで動かす
     this.movedown = function() {
     };
 
+    game.rootScene.addChild(this);
   },
 
   remove : function() {
+    game.rootScene.removeChild(this);
+    delete this;
   }
+
 });
